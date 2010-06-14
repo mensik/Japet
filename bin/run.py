232c53968df2a694fcf8cbc16baa3dca9a140689
Martin Mensik <martin.mensik@vsb.cz>
2010-06-14 14:30:31 +0200

    Enhance starting python script

--- bin/run.py
+++ bin/run.py
@@ -1,6 +1,11 @@
-import subprocess
+#! /usr/bin/env python
+
+import subprocess, ConfigParser
 
 print "***********************************"
+
+config = ConfigParser.SafeConfigParser()
+config.read('default.cfg')
 
 programs = ['testFeti', 'testSmale']
 i = 0
@@ -8,20 +13,42 @@ i = 0
 for p in programs:
 	print i,":", p, "\t",
 	i = i + 1
+try:
+	i = int(raw_input("\nSelect program: (0) "))
+except ValueError:
+	i = 0;
+try:
+	selectedProgram = programs[i]
+except IndexError:
+	selectedProgram = programs[0]
+	print "Ilegal selection. Default value (", selectedProgram ,") chosen."
 
-i = int(raw_input("\n\nSelect program: "))
-selectedProgram = programs[i]
+x = ["Number of domain x","grid","x","-test_x", int]
+y = ["Number of domain y","grid","y","-test_y", int]
+h = ["Grid step","grid","h","-test_h", float]
+d_count = ["Number of Dirchlet bounds","conditions","dirchlet_side_count","-test_bounded_side_count", int]
 
+args = [x,y,h,d_count]
 
-x = ["Number of domain x", "-test_x", int, 2]
-y = ["Number of domain y", "-test_y", int, 2]
-h = ["Grid step", "-test_h", float, 2]
-args = [x,y,h]
 for arg in args:
-	arg[4:4] = [arg[2](raw_input(arg[0] + ": (" + str(arg[3]) + ") "))]
+	try:
+		if arg[4] == int:
+			arg[5:5] = [config.getint(arg[1],arg[2])]
+		elif arg[4] == float:
+			arg[5:5] = [config.getfloat(arg[1],arg[2])]
+		
+		arg[5] = arg[4](raw_input(arg[0] + ": (" + str(arg[5]) + ") "))
+	except ValueError:
+		pass	
 
-carr = ["mpiexec", "-n", str(args[0][4] * args[1][4]), "./" + selectedProgram]
+carr = ["mpiexec", "-n", str(args[0][5] * args[1][5]), "./" + selectedProgram]
 for arg in args:
-	carr = carr + [arg[1], str(arg[4])]
+	carr = carr + [arg[3], str(arg[5])]
+	config.set(arg[1],arg[2],str(arg[5]))
+
+with open('default.cfg','w') as f:
+	config.write(f)
+
+print "***********************************"
 
 subprocess.call(carr)
