21f06e81362854541dc1c39145cb03e5fccfeaff
Martin Mensik <martin.mensik@vsb.cz>
2011-07-19 17:00:53 +0200

    Lesson1

--- lessons/src/dayOne.cpp
+++ lessons/src/dayOne.cpp
@@ -11,50 +11,16 @@
 
 using namespace std;
 
+double norm(int len, double *vec);
+
 int main(int argc, char *argv[]) {
 
-	PetscInitialize(&argc, &argv, 0, "Day one");
+	double a[10];
 
-	int rank, size;
-
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-	Vec a, b, c;
-
-	VecCreateMPI(PETSC_COMM_WORLD, PETSC_DECIDE, 10, &a);
-	VecDuplicate(a, &b);
-	VecDuplicate(a, &c);
-
-	VecSet(a, 1);
-
-	if (!rank) {
-		for (int i = 0; i < 10; i++) {
-			VecSetValue(b, i, i*i, INSERT_VALUES);
-		}
+	for (int i = 0; i < 10; i++) {
+		a[i] = i + 1;
 	}
 
-	VecAssemblyBegin(b);
-	VecAssemblyEnd(b);
-
-
-	PetscViewer v;
-	PetscViewerBinaryOpen(PETSC_COMM_WORLD, "b.m", FILE_MODE_WRITE, &v);
-	VecView(b, v);
-	PetscViewerDestroy(v);
-
-	PetscReal normB;
-	VecNorm(b, NORM_2, &normB);
-
-	PetscPrintf(PETSC_COMM_WORLD, "b ma normu %f \n", normB);
-
-	if (!rank) {
-		PetscPrintf(PETSC_COMM_SELF, "Ale jen ja jsem master! \n");
-	}
-
-	PetscPrintf(PETSC_COMM_WORLD, "Mej se \n", rank, size);
-
-	PetscFinalize();
 
 	return 0;
 }
